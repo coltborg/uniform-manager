1c3556bca321ecf90990d90fc70a07ee99251ffd
coltborg <coltborg@gmail.com>
2018-04-29 12:17:39 -0500

    Add grid to students list
    Add global header

--- pages/students/index.vue
+++ pages/students/index.vue
@@ -1,18 +1,28 @@
 <template lang="pug">
-  section
-    h1 Students Listing
-    .studentsListing
-      student-short(
-        v-for="student in students.entities.students"
-        :key="student.id"
-        :student="student"
-        v-on:studentClicked="handleClickedStudent"
-      )
+  div
+    section.mt-8.mx-4
+      h1.mt-0.mb-4.text-3xl.font-bold Students
+      div
+        .grid.bg-grey-lightest
+          .p-2.text-grey-dark.font-bold Student Name
+          .p-2.text-grey-dark.font-bold Bibber
+          .p-2.text-grey-dark.font-bold Shirts
+          .p-2.text-grey-dark.font-bold Gauntlets
+          .p-2.text-grey-dark.font-bold Jackets
+          .p-2.text-grey-dark.font-bold Shakos
+          .p-2.text-grey-dark.font-bold Track Suits
+          .p-2.text-grey-dark.font-bold Uniform Bags
+        student-short(
+          v-for="student in students.entities.students"
+          :key="student.id"
+          :student="student"
+          v-on:studentClicked="handleClickedStudent"
+        )
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import studentShort from '~/components/studentShort.vue'
+import { mapState } from "vuex";
+import studentShort from "~/components/studentShort.vue";
 
 export default {
   components: {
@@ -22,24 +32,31 @@ export default {
   data: () => {
     return {
       activeStudent: {}
-    }
+    };
   },
 
   computed: {
-    ...mapState('students', {
-      'students': state => state.students,
-      'studentRaw': state => state.studentsRaw
+    ...mapState("students", {
+      students: state => state.students,
+      studentRaw: state => state.studentsRaw
     })
   },
 
   methods: {
     handleClickedStudent(student) {
-      this.activeStudent = student
+      this.activeStudent = student;
     }
   }
-}
+};
 </script>
 
-<style>
-
+<style lang="postcss" scoped>
+.grid {
+  display: grid;
+  grid-template:
+    1fr
+    / 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-column-gap: 1rem;
+  grid-row-gap: 0.5rem;
+}
 </style>
