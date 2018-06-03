4699c964e3e5e6e60fc529ef833dd7c673e3f728
coltborg <coltborg@gmail.com>
2018-06-03 08:28:27 -0500

    Add workspace vscode settings for fixOnSave

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,72 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'airbnb-base',
+    'plugin:vue/recommended'
   ],
   // required to lint *.vue files
   plugins: [
     'vue'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    // remove max-len rule
+    'max-len': [0],
+    // don't require .vue extension when importing
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-unused-expressions': ['error', {'allowTernary': true}],
+    'no-console': 'warn',
+    'no-alert': 'warn',
+    'no-param-reassign': 0,
+    'quote-props': ['error', 'consistent'],
+    // vue rules
+    'vue/order-in-components': ['error', {
+      'order': [
+        'el',
+        'name',
+        'parent',
+        'functional',
+        ['delimiters', 'comments'],
+        ['components', 'directives', 'filters'],
+        'extends',
+        'mixins',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'data',
+        'computed',
+        'watch',
+        'LIFECYCLE_HOOKS',
+        'methods',
+        ['template', 'render'],
+        'renderError'
+      ]
+    }],
+    'vue/html-closing-bracket-newline': ['error', {
+      'singleline': 'never',
+      'multiline': 'never'
+    }],
+    'vue/html-closing-bracket-spacing': ['error', {
+      'startTag': 'never',
+      'endTag': 'never',
+      'selfClosingTag': 'always'
+    }],
+    'vue/script-indent': ['error', 2, {
+      'baseIndent': 0,
+      'switchCase': 1,
+      'ignores': []
+    }],
+    'vue/max-attributes-per-line': [2, {
+      'singleline': 1,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': false
+      }
+    }]
+  }
 }
